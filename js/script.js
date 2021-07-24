25248f19235b9873436a9b7fd2ccfecb2efdc6b1
sekoviper <damehlivingstone1@gmail.com>
2021-07-23 19:11:44 -0700

    nav menu edit

--- js/script.js
+++ js/script.js
@@ -6,10 +6,19 @@ const openMenu = document.querySelector('.openMenu');
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
 
+// openMenu.addEventListener('click',show);
+// closeMenu.addEventListener('click',close);
+
 function show(){
     mainMenu.style.display = 'flex';
     mainMenu.style.top = '0';
 }
 function close(){
     mainMenu.style.top = '-100%';
+}
+
+
+function hideMenu() {
+    var menuItem = document.getElementById("mainMenu");
+    menuItem.style.display = "block";  // <-- Set it to block
 }
