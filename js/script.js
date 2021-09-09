a7d04ee7bed42cde29314c8023ac40e632f03910
Divine Dela <divinedela.dd@gmail.com>
2021-09-09 16:32:37 +0000

    add ui updates

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 /* Open */
 window.openMenu = function openMenu() {
-    getMenu().style.display = "block";
+    const menu = getMenu();
+    console.log()
+    menu.style.display = "block";
+    menu.style.height = "100vh"
     hideMenuBtn();
     showCloseBtn();
     window.document.body.style.overflowY = 'hidden';
+    window.document.body.style.position = 'fixed';
+    window.document.body.style.left = '2.6rem';
 }
 
 function showMenuBtn() {
@@ -34,10 +39,14 @@ window.goToShopDetail = function () {
 
 /* close menu */
 window.closeMenu = function closeMenu() {
-    getMenu().style.display = "none";
+    const menu = getMenu();
+    menu.style.height = "0vh"
+    menu.style.display = "none";
     hideCloseBtn();
     showMenuBtn();
     window.document.body.style.overflowY = 'scroll';
+    window.document.body.style.position = '';
+    window.document.body.style.left = '0';
 }
 
 
